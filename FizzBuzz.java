aa22c8d2486d6c903f0977271c353f1efad14fcc
James Easton <james.easton@stu.jefferson.kyschools.us>
2015-11-18 16:12:40 +0000

    Created descrition of functions ofFizzBuzz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,6 +3,15 @@ class FizzBuzz {
   //will inlude methods for computing a single FizzBuzz number
   //and a list of n FizzBuzz numbers
 
-  // fizzNumber (int n)
+  /* fizzNumber (int n)
+  Computes the proper fizzbuzz output
+  for a number, n
+  */
+
+  /*fizzList(int start, int end)
+  Creates a string containing
+  a list of all fizzbuzz outputs starting with
+  start and ending with end -1
+  */
 
 }
